6e878566735c1fb15f80838c33a9a9d72d1f4255
hongjianlin1989 <ttzb1989@yahoo.com>
2017-05-13 11:42:15 -0400

    first commit

---
+++ instagramDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagramDemo
+//
+//  Created by hongjian lin on 5/13/17.
+//  Copyright © 2017 com.hongjian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
