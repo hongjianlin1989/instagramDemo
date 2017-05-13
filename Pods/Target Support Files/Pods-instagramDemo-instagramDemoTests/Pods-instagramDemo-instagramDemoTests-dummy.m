d17437d1ce23c4fab361fe99b4029ffa1a5b2e25
hongjianlin1989 <ttzb1989@yahoo.com>
2017-05-13 11:44:47 -0400

    pod file

---
+++ Pods/Target Support Files/Pods-instagramDemo-instagramDemoTests/Pods-instagramDemo-instagramDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_instagramDemo_instagramDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_instagramDemo_instagramDemoTests
+@end
---
+++ Pods/Target Support Files/Pods-instagramDemo-instagramDemoUITests/Pods-instagramDemo-instagramDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_instagramDemo_instagramDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_instagramDemo_instagramDemoUITests
+@end
