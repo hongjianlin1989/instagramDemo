e6ef97c0247c3bfa8ba396df0b98707bd437d8be
hongjianlin1989 <ttzb1989@yahoo.com>
2017-05-14 10:37:17 -0400

    detail view

--- instagramDemo/DetailViewController.swift
+++ instagramDemo/DetailViewController.swift
@@ -1,21 +1,38 @@
 //
-//  ViewController.swift
+//  DetailViewController.swift
 //  instagramDemo
 //
-//  Created by hongjian lin on 5/13/17.
+//  Created by hongjian lin on 5/14/17.
 //  Copyright © 2017 com.hongjian. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
-    
-    
-    @IBOutlet var collectionView: UICollectionView!
+class DetailViewController: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+
     
     let reuseIdentifier = "FeedCell"
     
-    var feedArray : Array<Feed> = Array.init()
+    var feed: Feed?
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.register(UINib(nibName: "FeedCell", bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.reloadData()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func CloseButtonClick(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
     // MARK: - UICollectionViewDelegate protocol
     
@@ -29,14 +46,14 @@ class ViewController: UIViewController, UICollectionViewDelegate,UICollectionVie
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return feedArray.count
+        return 1
     }
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! FeedCell
-        let feedVM = FeedCellViewModel.init(inputFeed: feedArray[indexPath.row])
+        let feedVM = FeedCellViewModel.init(inputFeed: feed!)
         cell.nameLabel.text = feedVM.userName
         cell.descriptionField.text = feedVM.bodyDescription
         cell.subTitleLabel.text = feedVM.timeCreated
@@ -55,45 +72,18 @@ class ViewController: UIViewController, UICollectionViewDelegate,UICollectionVie
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
-        let senderFeed = feedArray[indexPath.row]
-        self.performSegue(withIdentifier: "DetailViewController", sender: senderFeed)
-    }
-    
-    // MARK: - NewWorking
-    
-    func getListOfFeed(){
-        
-        Feed.getListOfFeed(params: ["access_token" : DefaultSettings.sharedInstance.token]) { (feedArray, Error) in
-            self.feedArray = feedArray
-            self.collectionView.reloadData()
-            
-        }
         
     }
     
-    // MARK: - View State
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        collectionView.register(UINib(nibName: "FeedCell", bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
-        getListOfFeed()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        if segue.identifier == "DetailViewController" {
-            let detailVC = segue.destination as! DetailViewController
-            detailVC.feed = sender as? Feed
-        }
-
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
-    
-}
+    */
 
+}
